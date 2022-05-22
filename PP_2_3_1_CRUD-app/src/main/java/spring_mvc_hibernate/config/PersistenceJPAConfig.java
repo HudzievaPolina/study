26bd553b7396528594af9626365b4da7436ecd65
HudzievaPolina <p.hirundo@gmail.com>
2022-05-22 20:12:44 +0300

    task 2.3.1

--- PP_2_3_1_CRUD-app/src/main/java/spring_mvc_hibernate/config/PersistenceJPAConfig.java
+++ PP_2_3_1_CRUD-app/src/main/java/spring_mvc_hibernate/config/PersistenceJPAConfig.java
@@ -1,35 +1,41 @@
 package spring_mvc_hibernate.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
-public class PersistenceJPAConfig{
+//@PropertySource("classpath:db.properties")
 
+public class PersistenceJPAConfig{
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean entityManagerFactory
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+        LocalContainerEntityManagerFactoryBean em
                 = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactory.setDataSource(dataSource());
-        entityManagerFactory.setPackagesToScan(new String[] { "spring_mvc_hibernate" });
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(new String[] { "spring_mvc_hibernate" });
 
         JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
-        entityManagerFactory.setJpaProperties(additionalProperties());
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
 
-        return entityManagerFactory;
+        return em;
     }
     @Bean
     public DataSource dataSource(){
@@ -43,7 +49,7 @@ public class PersistenceJPAConfig{
     @Bean
     public PlatformTransactionManager transactionManager() {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        transactionManager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
 
         return transactionManager;
     }
@@ -62,3 +68,100 @@ public class PersistenceJPAConfig{
     }
 
 }
+
+    //-------------------------------------------------------------------------------------------
+
+    //    @Autowired
+//    private Environment environment;
+//
+//    @Bean(name = "entityManagerFactory")
+//    public LocalContainerEntityManagerFactoryBean getEntityManagerFactoryBean() {
+//        LocalContainerEntityManagerFactoryBean lcemfb = new LocalContainerEntityManagerFactoryBean();
+//        lcemfb.setJpaVendorAdapter(getJpaVendorAdapter());
+//        lcemfb.setDataSource(dataSource());
+//        lcemfb.setPersistenceUnitName("mydbtest");
+//        lcemfb.setPackagesToScan("spring_mvc_hibernate");
+//        lcemfb.setJpaProperties(hibernateProperties());
+//        return lcemfb;
+//    }
+//
+//    @Bean
+//    public JpaVendorAdapter getJpaVendorAdapter() {
+//        JpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+//        return adapter;
+//    }
+//
+//    @Bean(name = "transactionManager")
+//    public PlatformTransactionManager txManager() {
+//        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager(
+//                getEntityManagerFactoryBean().getObject());
+//        return jpaTransactionManager;
+//    }
+//
+//    /************* End Spring JPA config details **************/
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName(environment.getRequiredProperty("db.driver"));
+//        dataSource.setUrl(environment.getRequiredProperty("db.url"));
+//        dataSource.setUsername(environment.getRequiredProperty("db.username"));
+//        dataSource.setPassword(environment.getRequiredProperty("db.password"));
+//        return dataSource;
+//    }
+//
+//    private Properties hibernateProperties() {
+//        Properties properties = new Properties();
+//        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+//        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+//        return properties;
+//    }
+//
+//}
+
+//---------------------------------------------------------------------------------------------------
+
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+//        LocalContainerEntityManagerFactoryBean entityManagerFactory
+//                = new LocalContainerEntityManagerFactoryBean();
+//        entityManagerFactory.setDataSource(dataSource());
+//        entityManagerFactory.setPackagesToScan(new String[] { "spring_mvc_hibernate" });
+//
+//        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//        entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
+//        entityManagerFactory.setJpaProperties(additionalProperties());
+//
+//        return entityManagerFactory;
+//    }
+//    @Bean
+//    public DataSource dataSource(){
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/mydbtest");
+//        dataSource.setUsername( "root" );
+//        dataSource.setPassword( "asyasweetcream" );
+//        return dataSource;
+//    }
+//    @Bean
+//    public PlatformTransactionManager transactionManager() {
+//        JpaTransactionManager transactionManager = new JpaTransactionManager();
+//        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+//
+//        return transactionManager;
+//    }
+//
+//    @Bean
+//    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+//        return new PersistenceExceptionTranslationPostProcessor();
+//    }
+//
+//    Properties additionalProperties() {
+//        Properties properties = new Properties();
+//        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+//        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+//
+//        return properties;
+//    }
+//
+//}
--- untitled/src/main/java/DAO.java
+++ untitled/src/main/java/DAO.java
@@ -1,44 +1,39 @@
-package spring_mvc_hibernate.dao;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.stereotype.Repository;
-import spring_mvc_hibernate.entity.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
-public class UserDAOImpl implements UserDAO{
+public class UserDAO{
 
     @Autowired
-    private LocalContainerEntityManagerFactoryBean entityManagerFactoryBean;
-    private EntityManagerFactory entityManagerFactory = entityManagerFactoryBean.getObject();
+    private LocalContainerEntityManagerFactoryBean entityManagerFactory;
 
-    @Override
+
     public List<User> getAllUsers() {
 
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityManager entityManager = entityManagerFactory.();
         List<User> users = entityManager.createQuery("from User", User.class).getResultList();
 
         return users;
     }
 
-    @Override
+
     public void saveUser(User user) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         entityManager.merge(user);
     }
 
-    @Override
+
     public User getUserById(long id) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         return entityManager.find(User.class, id);
     }
 
-    @Override
+
     public void deleteUser(long id) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         entityManager.remove(entityManager.find(User.class, id));
