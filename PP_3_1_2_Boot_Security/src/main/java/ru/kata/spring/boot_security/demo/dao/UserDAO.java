963c5e5735f16d5a6b09a09f01aac8cb90c25e78
HudzievaPolina <55093180+HudzievaPolina@users.noreply.github.com>
2022-06-06 13:02:47 +0300

    Add files via upload

---
+++ PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/dao/UserDAO.java
@@ -0,0 +1,16 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+import ru.kata.spring.boot_security.demo.model.User;
+import java.util.List;
+
+public interface UserDAO {
+    public List<User> getAllUsers();
+
+    void saveUser(User user);
+
+    User getUserById(long id);
+
+    void deleteUser(long id);
+
+    User getUserByEmail(String email);
+}
---
+++ PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -0,0 +1,55 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.dao.UserDAO;
+import ru.kata.spring.boot_security.demo.model.User;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserServiceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    @Transactional
+    public List<User> getAllUsers() {
+        return userDAO.getAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        userDAO.saveUser(user);
+    }
+
+    @Override
+    @Transactional
+    public User getUserById(long id) {
+        return userDAO.getUserById(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(long id) {
+        userDAO.deleteUser(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userDAO.getUserByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
+}
