7e2ffe926c72f30826805cb6bb8fd942800bf6e5
HudzievaPolina <55093180+HudzievaPolina@users.noreply.github.com>
2022-06-14 12:56:52 +0300

    task 3.1.3 corrected

---
+++ PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -0,0 +1,47 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String role;
+
+    public Role() {}
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getRole();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return  role;
+    }
+}
